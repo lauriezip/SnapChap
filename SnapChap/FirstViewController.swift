484fae21d8acd95f76a95d83c39e6619f6983fcb
lauriezip <lauriezip@gmail.com>
2017-10-27 13:55:53 -0400

    Initial Commit

---
+++ SnapChap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SnapChap
+//
+//  Created by Laurie Zipperer on 10/27/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
